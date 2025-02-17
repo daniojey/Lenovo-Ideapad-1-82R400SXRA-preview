e1c3fbcb138c48a1d1a44e67ae87484dc75b46ed
Saymios <daniojenezis@gmail.com>
2025-02-17 19:57:35 +0200

    create base structure project

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from main import views
+
+app_name = "main"
+
+urlpatterns = [
+    path('', views.homepage, name="homepage")
+]
